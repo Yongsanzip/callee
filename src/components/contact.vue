c9901eb3edc93c67d8286bfbe07272fbc972da85
bobby1815 <bobby1815@nate.com>
2020-05-18 16:56:05 +0900

    fix

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,25 +3,25 @@
         <div class="bgImg pc">
         </div>
         <div class="formPanel">
-            <form ref="mailForm" @submit.prevent="sendEmail">
+            <form ref="mailForm" @submit.prevent="sendEmail(this)">
                 <p>CONTACT US</p>
                 <div>
-                    <input type="text" placeholder="성명" name="name" />
-                    <input type="text" placeholder="회사명" name="company" />
+                    <input type="text" placeholder="성명" ref="name" />
+                    <input type="text" placeholder="회사명" ref="company" />
                 </div>
                 <div>
-                    <input type="text" placeholder="연락처" name="phone" />
-                    <input type="text" placeholder="이메일" name="email" />
+                    <input type="text" placeholder="연락처" ref="phone" />
+                    <input type="text" placeholder="이메일" ref="email" />
                 </div>
-                <div><textarea name="content" placeholder="내용을 입력해주세요" /></div>
-                <div>
+                <div><textarea ref="content" placeholder="내용을 입력해주세요" /></div>
+                <!--<div>
                     <button class="fileupload" @click="clickFileUploadBtn">파일첨부</button>
                     <input type="file" ref="fileInput" style="display: none;">
                     <span class="filename">파일명.png</span>
-                </div>
+                </div>-->
                 <div>
                     <input type="checkbox" name="check">
-                    <div class="personalCheckContent">개인정보 수집, 이용에 동의, 상담용도로만 이용합니다. <a href="#">자세히보기</a></div>
+                    <div class="personalCheckContent">개인정보 수집, 이용에 동의, 상담용도로만 이용합니다. </div>
                 </div>
                 <button type="submit" class="submitBtn">SEND</button>
             </form>
@@ -37,14 +37,15 @@
             clickFileUploadBtn() {
                 this.$refs.fileInput.click();
             },
-            sendEmail: (e) => {
+            sendEmail() {
                 const templateParams = {
-                    name: e.name.name,
-                    company: e.name.company,
-                    phone:e.name.phone,
-                    email:e.name.email,
+                    name: this.$refs.name.value,
+                    company: this.$refs.company.value,
+                    phone:this.$refs.phone.value,
+                    email:this.$refs.email.value,
+                    content:this.$refs.content.value,
                 };
-                emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams, 'YOUR_USER_ID')
+                emailjs.send('shhphone', 'callee_contract', templateParams, 'user_MKoM4BpLU5gKYwZ75IO26')
                     .then((result) => {
                         console.log('SUCCESS!', result.status, result.text);
                     }, (error) => {
